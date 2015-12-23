b1a48f892f7ae627608523a2eaf4b64df497b3e2
ghaiklor <ghaiklor@gmail.com>
2015-12-23 16:36:30 +0200

    docs(cursor): Add example for http server

---
+++ examples/http.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var Cursor = require('../lib/Cursor').Cursor;
+var radius = 10;
+var theta = 0;
+var points = [];
+
+http.createServer(function (req, res) {
+  res.setHeader('content-type', 'text/ansi');
+
+  var cursor = new Cursor(res).resetTTY();
+  var colors = [cursor.COLORS.RED, cursor.COLORS.YELLOW, cursor.COLORS.GREEN, cursor.COLORS.DARK_CYAN, cursor.COLORS.BLUE, cursor.COLORS.MAGENTA];
+
+  setInterval(function () {
+    var x = 2 + (radius + Math.cos(theta) * radius) * 2;
+    var y = 2 + radius + Math.sin(theta) * radius;
+
+    points.unshift([x, y]);
+    points.forEach(function (p, i) {
+      cursor.moveTo(p[0], p[1]);
+      cursor.background(colors[Math.floor(i / 12)]).write(' ').flush();
+    });
+
+    points = points.slice(0, 12 * colors.length - 1);
+
+    theta += Math.PI / 40;
+  }, 50);
+}).listen(8081);
