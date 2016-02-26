72fb136ba8090083f8fc139a24d028481a27024e
ghaiklor <ghaiklor@gmail.com>
2016-02-26 10:59:44 +0200

    test(cursor): Fix tests for image()

--- test/unit/Cursor.test.js
+++ test/unit/Cursor.test.js
@@ -399,7 +399,7 @@ describe('Cursor', () => {
     const cursor = new Cursor();
     const mock = sinon.mock(process.stdout);
 
-    mock.expects('write').once().withExactArgs(new Buffer('\u001b[0;0f\u001b]1337;File=width=auto;height=auto;preserveAspectRatio=1;inline=1:base64Image^G'));
+    mock.expects('write').once().withExactArgs(new Buffer('\u001b[1;1f') + new Buffer('\u001b]1337;File=width=auto;height=auto;preserveAspectRatio=1;inline=1:base64Image^G'));
 
     assert.instanceOf(cursor.image({image: 'base64Image'}), Cursor);
     mock.verify();
@@ -409,7 +409,7 @@ describe('Cursor', () => {
     const cursor = new Cursor();
     const mock = sinon.mock(process.stdout);
 
-    mock.expects('write').once().withExactArgs(new Buffer('\u001b[0;0f\u001b]1337;File=width=200px;height=200px;preserveAspectRatio=1;inline=1:base64Image^G'));
+    mock.expects('write').once().withExactArgs(new Buffer('\u001b[1;1f') + new Buffer('\u001b]1337;File=width=200px;height=200px;preserveAspectRatio=1;inline=1:base64Image^G'));
 
     assert.instanceOf(cursor.image({image: 'base64Image', width: '200px', height: '200px'}), Cursor);
     mock.verify();
