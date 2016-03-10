58499000040acb6e8e8be9a444d4dbdd564f7774
ghaiklor <ghaiklor@gmail.com>
2016-03-10 16:24:53 +0200

    test(cursor): Add few tests

--- test/unit/Cursor.test.js
+++ test/unit/Cursor.test.js
@@ -7,6 +7,7 @@ describe('Cursor', () => {
     const cursor = new Cursor();
 
     assert.instanceOf(cursor, Cursor);
+    assert.equal(cursor._stream, process.stdout);
     assert.equal(cursor._width, process.stdout.columns);
     assert.equal(cursor._height, process.stdout.rows);
     assert.equal(cursor._x, 0);
@@ -19,44 +20,63 @@ describe('Cursor', () => {
     assert.notOk(cursor._display.blink);
     assert.notOk(cursor._display.reverse);
     assert.notOk(cursor._display.hidden);
-    assert.equal(cursor._terminal.length, process.stdout.columns * process.stdout.rows);
-    assert.equal(cursor._lastFrame.size, 1);
+    assert.equal(cursor._cells.length, process.stdout.columns * process.stdout.rows);
+  });
+
+  it('Should properly initialize with custom arguments', () => {
+    const cursor = new Cursor({stream: process.stdout, width: 10, height: 20});
+
+    assert.instanceOf(cursor, Cursor);
+    assert.equal(cursor._stream, process.stdout);
+    assert.equal(cursor._width, 10);
+    assert.equal(cursor._height, 20);
+    assert.equal(cursor._x, 0);
+    assert.equal(cursor._y, 0);
+    assert.notOk(cursor._background);
+    assert.notOk(cursor._foreground);
+    assert.notOk(cursor._display.bold);
+    assert.notOk(cursor._display.dim);
+    assert.notOk(cursor._display.underlined);
+    assert.notOk(cursor._display.blink);
+    assert.notOk(cursor._display.reverse);
+    assert.notOk(cursor._display.hidden);
+    assert.equal(cursor._cells.length, 10 * 20);
   });
 
   it('Should properly write to the cursor', () => {
     const cursor = new Cursor();
 
-    assert.equal(cursor._terminal[0], ' ');
+    assert.equal(cursor._cells[0].getChar(), ' ');
 
     cursor.write('test');
-    assert.equal(cursor._terminal[0], '\u001b[1;1ft\u001b[0m');
-    assert.equal(cursor._terminal[1], '\u001b[1;2fe\u001b[0m');
-    assert.equal(cursor._terminal[2], '\u001b[1;3fs\u001b[0m');
-    assert.equal(cursor._terminal[3], '\u001b[1;4ft\u001b[0m');
+    assert.equal(cursor._cells[0].toString(), '\u001b[1;1ft\u001b[0m');
+    assert.equal(cursor._cells[1].toString(), '\u001b[1;2fe\u001b[0m');
+    assert.equal(cursor._cells[2].toString(), '\u001b[1;3fs\u001b[0m');
+    assert.equal(cursor._cells[3].toString(), '\u001b[1;4ft\u001b[0m');
   });
 
   it('Should properly ignore write if out of the bounding box', () => {
     const cursor = new Cursor();
 
-    assert.equal(cursor._terminal[0], ' ');
+    assert.equal(cursor._cells[0].getChar(), ' ');
 
     cursor.write('test');
-    assert.equal(cursor._terminal[0], '\u001b[1;1ft\u001b[0m');
-    assert.equal(cursor._terminal[1], '\u001b[1;2fe\u001b[0m');
-    assert.equal(cursor._terminal[2], '\u001b[1;3fs\u001b[0m');
-    assert.equal(cursor._terminal[3], '\u001b[1;4ft\u001b[0m');
+    assert.equal(cursor._cells[0].toString(), '\u001b[1;1ft\u001b[0m');
+    assert.equal(cursor._cells[1].toString(), '\u001b[1;2fe\u001b[0m');
+    assert.equal(cursor._cells[2].toString(), '\u001b[1;3fs\u001b[0m');
+    assert.equal(cursor._cells[3].toString(), '\u001b[1;4ft\u001b[0m');
 
     cursor.moveTo(-5, -5).write('do not print');
-    assert.equal(cursor._terminal[0], '\u001b[1;1ft\u001b[0m');
-    assert.equal(cursor._terminal[1], '\u001b[1;2fe\u001b[0m');
-    assert.equal(cursor._terminal[2], '\u001b[1;3fs\u001b[0m');
-    assert.equal(cursor._terminal[3], '\u001b[1;4ft\u001b[0m');
-    assert.equal(cursor._terminal[4], ' ');
+    assert.equal(cursor._cells[0].toString(), '\u001b[1;1ft\u001b[0m');
+    assert.equal(cursor._cells[1].toString(), '\u001b[1;2fe\u001b[0m');
+    assert.equal(cursor._cells[2].toString(), '\u001b[1;3fs\u001b[0m');
+    assert.equal(cursor._cells[3].toString(), '\u001b[1;4ft\u001b[0m');
+    assert.equal(cursor._cells[4].getChar(), ' ');
   });
 
   it('Should properly flush the buffer into the stream', () => {
     const cursor = new Cursor();
-    const mock = sinon.mock(process.stdout);
+    const mock = sinon.mock(cursor._stream);
 
     mock.expects('write').once();
 
@@ -176,7 +196,9 @@ describe('Cursor', () => {
 
     assert.equal(cursor._foreground, false);
     assert.instanceOf(cursor.foreground('white'), Cursor);
-    assert.equal(cursor._foreground, 'white');
+    assert.deepEqual(cursor._foreground, {r: 255, g: 255, b: 255});
+    assert.instanceOf(cursor.foreground(false), Cursor);
+    assert.notOk(cursor._foreground);
   });
 
   it('Should properly change background color', () => {
@@ -184,7 +206,9 @@ describe('Cursor', () => {
 
     assert.equal(cursor._background, false);
     assert.instanceOf(cursor.background('black'), Cursor);
-    assert.equal(cursor._background, 'black');
+    assert.deepEqual(cursor._background, {r: 0, g: 0, b: 0});
+    assert.instanceOf(cursor.background(false), Cursor);
+    assert.notOk(cursor._background);
   });
 
   it('Should properly enable bold mode', () => {
@@ -245,7 +269,7 @@ describe('Cursor', () => {
     const cursor = new Cursor();
     const mock = sinon.mock(cursor);
 
-    mock.expects('getPointerFromXY').exactly(36).returns(cursor);
+    mock.expects('getPointerFromXY').exactly(36).returns(0);
 
     assert.instanceOf(cursor.moveTo(5, 5).erase(0, 0, 5, 5), Cursor);
     mock.verify();
@@ -311,6 +335,26 @@ describe('Cursor', () => {
     mock.verify();
   });
 
+  it('Should properly save the screen contents', () => {
+    const cursor = new Cursor();
+    const mock = sinon.mock(cursor._stream);
+
+    mock.expects('write').once().withExactArgs(new Buffer('\u001b[?47h'));
+
+    assert.instanceOf(cursor.saveScreen(), Cursor);
+    mock.verify();
+  });
+
+  it('Should properly restore the screen contents', () => {
+    const cursor = new Cursor();
+    const mock = sinon.mock(cursor._stream);
+
+    mock.expects('write').once().withExactArgs(new Buffer('\u001b[?47l'));
+
+    assert.instanceOf(cursor.restoreScreen(), Cursor);
+    mock.verify();
+  });
+
   it('Should properly hide the cursor', () => {
     const cursor = new Cursor();
     const mock = sinon.mock(process.stdout);
@@ -337,31 +381,15 @@ describe('Cursor', () => {
 
     mock.expects('write').once().withExactArgs(new Buffer('\u001bc'));
 
-    assert.instanceOf(cursor.resetTTY(), Cursor);
+    assert.instanceOf(cursor.reset(), Cursor);
     mock.verify();
-  });
-
-  it('Should properly wrap the char with control sequence', () => {
-    const x = 0;
-    const y = 0;
-    const background = 'black';
-    const foreground = 'white';
-    const display = {bold: true, underlined: true};
-
-    assert.equal(Cursor.wrap(' ', {x, y}), '\u001b[1;1f \u001b[0m');
-    assert.equal(Cursor.wrap(' ', {x, y, background}), '\u001b[1;1f\u001b[48;5;0m \u001b[0m');
-    assert.equal(Cursor.wrap(' ', {x, y, foreground}), '\u001b[1;1f\u001b[38;5;15m \u001b[0m');
-    assert.equal(Cursor.wrap(' ', {x, y, display}), '\u001b[1;1f\u001b[1m\u001b[4m \u001b[0m');
-  });
-
-  it('Should properly encode to VT100 compatible symbol', () => {
-    assert.deepEqual(Cursor.encodeToVT100('[J'), new Buffer('\u001b[J'));
   });
 
   it('Should properly create new instance from static create()', () => {
     const cursor = Cursor.create();
 
     assert.instanceOf(cursor, Cursor);
+    assert.equal(cursor._stream, process.stdout);
     assert.equal(cursor._width, process.stdout.columns);
     assert.equal(cursor._height, process.stdout.rows);
     assert.equal(cursor._x, 0);
@@ -374,7 +402,6 @@ describe('Cursor', () => {
     assert.notOk(cursor._display.blink);
     assert.notOk(cursor._display.reverse);
     assert.notOk(cursor._display.hidden);
-    assert.equal(cursor._terminal.length, process.stdout.columns * process.stdout.rows);
-    assert.equal(cursor._lastFrame.size, 1);
+    assert.equal(cursor._cells.length, process.stdout.columns * process.stdout.rows);
   });
 });
