0487327d6f285c05c53856a2f476f54116588741
ghaiklor <ghaiklor@gmail.com>
2016-03-14 22:48:17 +0200

    perf(util): Improve perfomance of encodeToVT100

--- src/util/encodeToVT100.js
+++ src/util/encodeToVT100.js
@@ -4,4 +4,4 @@
  * @param {String} code Control code that you want to encode
  * @returns {Buffer} Returns encoded bytes
  */
-export const encodeToVT100 = code => new Buffer([0x1b].concat(code.split('').map(char => char.charCodeAt(0))));
+export const encodeToVT100 = code => `\u001b${code}`;
