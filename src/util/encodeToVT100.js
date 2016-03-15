1fbef25c78e8677116d2bee0aea2a239b4653de6
ghaiklor <ghaiklor@gmail.com>
2016-03-15 13:58:14 +0200

    perf(util): Change template string to concatenation

--- src/util/encodeToVT100.js
+++ src/util/encodeToVT100.js
@@ -2,6 +2,6 @@
  * Bytes to encode to VT100 control sequence.
  *
  * @param {String} code Control code that you want to encode
- * @returns {Buffer} Returns encoded bytes
+ * @returns {String} Returns encoded string
  */
-export const encodeToVT100 = code => `\u001b${code}`;
+export const encodeToVT100 = code => '\u001b' + code;
