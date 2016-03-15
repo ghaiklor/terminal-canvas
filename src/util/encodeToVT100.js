b530486d24e984e200dc246dd08baf0de72c182f
ghaiklor <ghaiklor@gmail.com>
2016-03-15 17:21:57 +0200

    perf(util): Change back to template string
    IRHydra says that possible deopt can be in concatenation

--- src/util/encodeToVT100.js
+++ src/util/encodeToVT100.js
@@ -4,4 +4,4 @@
  * @param {String} code Control code that you want to encode
  * @returns {String} Returns encoded string
  */
-export const encodeToVT100 = code => '\u001b' + code;
+export const encodeToVT100 = code => `\u001b${code}`;
