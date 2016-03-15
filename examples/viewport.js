91245259f3931fd8cbee4b75318b80ceb376050d
ghaiklor <ghaiklor@gmail.com>
2016-03-15 19:15:13 +0200

    docs(cursor): Update doc block

--- examples/viewport.js
+++ examples/viewport.js
@@ -3,5 +3,5 @@
 const Cursor = require('../lib/Cursor');
 const cursor = Cursor.create({width: 5}).reset();
 
-// It will print only 4567 because you have negative X coordinate and width equal to 5
+// It will print only 34567 because you have negative X coordinate and width equal to 5
 cursor.moveTo(-2, 1).write('1234567890').flush();
