1695164a203ef22403f190367789cbbc8d031d4e
ghaiklor <ghaiklor@gmail.com>
2016-03-15 20:28:50 +0200

    docs(cursor): Add example with playing YouTube video

---
+++ examples/video.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const ytdl = require('ytdl-core');
+const Speaker = require('speaker');
+const asciiPixels = require('ascii-pixels');
+const Throttle = require('stream-throttle').Throttle;
+const pcmAudio = require('youtube-terminal/lib/pcm-audio');
+const ffmpeg = require('youtube-terminal/lib/ffmpeg');
+const RawImageStream = require('youtube-terminal/lib/raw-image-stream');
+const cursor = require('../lib/Cursor').create().saveScreen().reset().hideCursor();
+
+ytdl.getInfo('https://www.youtube.com/watch?v=dQw4w9WgXcQ', (error, info) => {
+  if (error) return console.error(error);
+
+  playAudio(info);
+  playVideo(info);
+});
+
+function playVideo(info) {
+  const options = {fps: 12, width: cursor._width, contrast: 50 * 2.55, invert: true};
+  const video = info.formats.filter(format => format.resolution === '144p' && format.audioBitrate === null).sort((a, b) => a.container === 'webm' ? -1 : 1)[0];
+  const m = video.size.match(/^(\d+)x(\d+)$/);
+  const videoSize = {width: m[1], height: m[2]};
+  const frameWidth = Math.round(options.width);
+  const frameHeight = Math.round(videoSize.height / (videoSize.width / frameWidth));
+  const frameSize = frameWidth * frameHeight * 3;
+  const splitRegex = new RegExp(`.{1,${options.width}}`, 'g');
+
+  ffmpeg.rawImageStream(video.url, options)
+    .pipe(new Throttle({rate: frameSize * 12}))
+    .pipe(new RawImageStream(frameSize))
+    .on('data', frameData => {
+      const imageData = {data: frameData, width: frameWidth, height: frameHeight, format: 'RGB24'};
+      const ascii = asciiPixels(imageData, options).slice(0, -1).match(splitRegex);
+
+      for (let y = 0; y < frameHeight; y++) cursor.moveTo(0, y).write(ascii[y + 1] || '');
+
+      cursor.flush();
+    })
+    .on('end', onExit);
+}
+
+function playAudio(info) {
+  const audio = info.formats.filter(format => format.resolution === null).sort((a, b) => b.audioBitrate - a.audioBitrate)[0];
+  const speaker = new Speaker();
+  const updateSpeaker = codec => {
+    speaker.channels = codec.audio_details[2] === 'mono' ? 1 : 2;
+    speaker.sampleRate = parseInt(codec.audio_details[1].match(/\d+/)[0], 10)
+  };
+
+  pcmAudio(audio.url).on('codecData', updateSpeaker).pipe(speaker);
+}
+
+function onExit() {
+  cursor.showCursor().restoreScreen();
+}
+
+process.on('exit', onExit);
