8466059afd27e519b54d70c0481d9ef4e03e010f
ghaiklor <ghaiklor@gmail.com>
2016-03-21 16:39:22 +0200

    docs(cursor): Update smart-render example

--- examples/smart-render.js
+++ examples/smart-render.js
@@ -7,11 +7,16 @@ let x = 0;
 let y = 0;
 
 setInterval(() => {
-  cursor.moveTo(1, 0).background('white').foreground('black').write('This example shows how fast builds difference between two frames');
+  cursor.moveTo(0, 0).background('white').foreground('black').write('This example shows how fast builds difference between two frames');
   cursor.moveTo(x, y).background('yellow').foreground('black').write('ABCDEFGHIJKLMNOP');
   cursor.moveTo(x * 2, y).background('yellow').foreground('black').write('ABCDEFGHIJKLMNOP');
   cursor.moveTo(x * 3, y).background('yellow').foreground('black').write('ABCDEFGHIJKLMNOP');
-  cursor.flush().eraseScreen();
+
+  cursor.flush();
+
+  cursor.erase(x, y, x + 16, y);
+  cursor.erase(x * 2, y, x * 2 + 16, y);
+  cursor.erase(x * 3, y, x * 3 + 16, y);
 
   x = x > process.stdout.columns ? 0 : x + 1;
   y = y > process.stdout.rows ? 0 : y + 1;
