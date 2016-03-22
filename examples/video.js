91583a3d796f3b8c84ce9ddc9e11101904d7e5c1
ghaiklor <ghaiklor@gmail.com>
2016-03-22 14:34:56 +0200

    docs(cursor): Update examples
    BREAKING CHANGE: Fully rewritten render engine

--- examples/video.js
+++ examples/video.js
@@ -2,42 +2,53 @@
 
 const ytdl = require('ytdl-core');
 const Speaker = require('speaker');
-const asciiPixels = require('ascii-pixels');
 const Throttle = require('stream-throttle').Throttle;
 const pcmAudio = require('youtube-terminal/lib/pcm-audio');
 const ffmpeg = require('youtube-terminal/lib/ffmpeg');
 const RawImageStream = require('youtube-terminal/lib/raw-image-stream');
 const cursor = require('../lib/Cursor').create();
 
-ytdl.getInfo('https://www.youtube.com/watch?v=dQw4w9WgXcQ', (error, info) => {
-  if (error) return console.error(error);
+const CHARACTERS = ' .,:;i1tfLCG08@'.split('');
 
-  playAudio(info);
-  playVideo(info);
-});
+function imageToAscii(data, width, height) {
+  var contrastFactor = 2.95;
+  var ascii = '';
+
+  for (var y = 0; y < height; y += 2) {
+    for (var x = 0; x < width; x++) {
+      var offset = (y * width + x) * 3;
+      var r = Math.max(0, Math.min(contrastFactor * (data[offset] - 128) + 128, 255));
+      var g = Math.max(0, Math.min(contrastFactor * (data[offset + 1] - 128) + 128, 255));
+      var b = Math.max(0, Math.min(contrastFactor * (data[offset + 2] - 128) + 128, 255));
+      var brightness = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+      ascii += CHARACTERS[Math.round(brightness * 14)];
+    }
+  }
+
+  return ascii;
+}
 
 function playVideo(info) {
-  const options = {fps: 12, width: cursor._width, contrast: 50 * 2.55, invert: true};
+  const options = {fps: 30, width: cursor._width};
   const video = info.formats.filter(format => format.resolution === '144p' && format.audioBitrate === null).sort((a, b) => a.container === 'webm' ? -1 : 1)[0];
   const m = video.size.match(/^(\d+)x(\d+)$/);
   const videoSize = {width: m[1], height: m[2]};
   const frameWidth = Math.round(options.width);
   const frameHeight = Math.round(videoSize.height / (videoSize.width / frameWidth));
   const frameSize = frameWidth * frameHeight * 3;
-  const splitRegex = new RegExp(`.{1,${options.width}}`, 'g');
 
   ffmpeg.rawImageStream(video.url, options)
-    .pipe(new Throttle({rate: frameSize * 12}))
+    .pipe(new Throttle({rate: frameSize * options.fps}))
     .pipe(new RawImageStream(frameSize))
-    .on('data', frameData => {
-      var ascii = asciiPixels({
-        data: frameData,
-        width: frameWidth,
-        height: frameHeight,
-        format: 'RGB24'
-      }, options).match(splitRegex);
+    .on('data', function (frameData) {
+      var ascii = imageToAscii(frameData, frameWidth, frameHeight);
 
-      for (var y = 0; y < frameHeight; y++) cursor.moveTo(0, y).write(ascii[y + 1] || '');
+      for (var y = 0; y < frameHeight; y++) {
+        for (var x = 0; x < frameWidth; x++) {
+          cursor.moveTo(x, y).write(ascii[y * frameWidth + x] || '');
+        }
+      }
 
       cursor.flush();
     });
@@ -53,3 +64,10 @@ function playAudio(info) {
 
   pcmAudio(audio.url).on('codecData', updateSpeaker).pipe(speaker);
 }
+
+ytdl.getInfo('https://www.youtube.com/watch?v=Hiqn1Ur32AE', (error, info) => {
+  if (error) return console.error(error);
+
+  playVideo(info);
+  playAudio(info);
+});
